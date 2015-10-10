c6df72ae04be2b36c5df88521b85b74088af77d1
NaisGames <nikola.szendzielorz@gmail.com>
2015-10-10 15:09:23 +0200

    all ships can be added
    TODO validation of size

--- BattleshipsGame/GameBoard.cs
+++ BattleshipsGame/GameBoard.cs
@@ -52,55 +52,33 @@ namespace BattleshipsGame
                 try
                 {
                     rowCoord = Convert.ToInt32(place.Substring(0, 1));
-                    //rowCoord = Convert.ToInt32(place[0]);
-                    colCoord = (((int)(place[1]) - 97) * 3)+1;
-
+                    colCoord = ((int)(place[1]) - 96);
                     if (place.Length == 5) {
                         rowCoord2 = Convert.ToInt32(place.Substring(3, 1));
-                        colCoord2= (((int)(place[4]) - 97) * 3)+1;
-                }
+                        colCoord2 = ((int)(place[4]) - 96);
+                    }
                 }
                 catch (Exception e) { Console.WriteLine(e.Message); }
-            
-            /*
-            switch (place.Substring(1, 1))
-            {
-                case "a":
-                    colCoord = 1;
-                    break;
-                case "b":
-                    colCoord = 2;
-                    break;
-                case "c":
-                    colCoord = 3;
-                    break;
-            }
-            
-            if (place.Length==5)
-            {
-                switch (place.Substring(place.Length-1, 1))
-                {
-                    case "a":
-                        colCoord2 = 1;
-                        break;
-                    case "b":
-                        colCoord2 = 2;
-                        break;
-                    case "c":
-                        colCoord2 = 3;
-                        break;
-                }
-            }*/
-
         }
 
         public void boardUpdate() {
             Console.Clear();
             emptyBoard[rowCoord+1, colCoord] = "0|";
+            if (colCoord == colCoord2)
+            {
+                for (int i = rowCoord; i <= rowCoord2; i++)
+                    emptyBoard[i+1,colCoord] = "0|";
+            }
+            if (rowCoord == rowCoord2)
+            {
+                for (int i = colCoord; i <= colCoord2; i++)
+                    emptyBoard[rowCoord+1, i] = "0|";
+            }
+            /*
             if (rowCoord2 != 0 && colCoord2!=0)
             {
                 emptyBoard[rowCoord2+1, colCoord2] = "0|";
-            }
+            }*/
             displayBoard();
         }
         
